c922b4fc3b3b6ba44dc57e8ebed351c0f3ef19bb
Pawel Paszki <pawelpaszki@gmail.com>
2018-02-24 17:57:39 +0000

    check OS version complete

--- src/controllers/miscController.ts
+++ src/controllers/miscController.ts
@@ -1,6 +1,9 @@
 import {Request, Response} from 'express';
+import * as lodash from 'lodash';
+import * as get from 'simple-get';
 import {ChildProcessHandler} from '../utilities/ChildProcessHandler';
 import ImageNameToDirNameConverter from '../utilities/ImageNameToDirNameConverter';
+import {default as OutputParser, IOsJSON} from '../utilities/OutputParser';
 
 class MiscController {
 
@@ -16,12 +19,11 @@ class MiscController {
           });
         } else {
           try {
-            const output: string = await ChildProcessHandler.executeChildProcCommand(
+            await ChildProcessHandler.executeChildProcCommand(
               'cd imagesTestDir && rm -rf ' + testDir, false);
             res.status(200).json({
               error: 'Source code successfully removed',
             });
-            /* istanbul ignore next */
           } catch (error) {
             res.status(500).json({
               error: 'Unable to remove source code',
@@ -42,7 +44,6 @@ class MiscController {
     const password: string = req.body.password;
     const dockerLoginOutput: string = await ChildProcessHandler.executeChildProcCommand(
       'docker login -u ' + username + ' -p ' + password, true);
-    /* istanbul ignore if */
     if (dockerLoginOutput.includes('Login Succeeded')) {
       res.status(200).json({
         message: 'Login Successful',
@@ -53,6 +54,58 @@ class MiscController {
       });
     }
   }
+
+  public checkOS = async (req: Request, res: Response) => {
+    const testDir: string = ImageNameToDirNameConverter.convertImageNameToDirName(req.body.imageName);
+    if (testDir.length > 0) {
+      async function checkOSVersion() {
+        const checkDirOutput: string = await ChildProcessHandler.executeChildProcCommand(
+          'cd imagesTestDir && find . -maxdepth 1 -name ' + testDir, false);
+        if (!checkDirOutput.includes(testDir)) {
+          return res.status(404).json({
+            error: 'No source code found',
+          });
+        } else {
+          try {
+            const path: string = 'imagesTestDir/' + testDir + '/etc/os-release';
+            const osVersion: IOsJSON = await OutputParser.getOSVersion(path);
+            if (osVersion.name !== '') {
+              const url: string = 'https://registry.hub.docker.com/v2/repositories/library/'
+                + osVersion.name + '/tags/';
+              let jsonResponse = null;
+              /* tslint:disable */
+              get.concat(url, function(err, response, data) {
+                /* tslint:enable */
+                jsonResponse = JSON.parse(data.toString());
+                const results = lodash.map(jsonResponse.results, 'name');
+                /* tslint:disable */
+                const osVersions  = results.filter(function(el) {
+                  return el.length && el == +el;
+                });
+                /* tslint:enable */
+                osVersions.sort();
+                res.status(200).json({
+                  latest: osVersions[osVersions.length - 1],
+                  name: osVersion.name,
+                  version: osVersion.version,
+                });
+              });
+            }
+          } catch (error) {
+            res.status(500).json({
+              error: 'Unable to get OS version',
+            });
+          }
+        }
+      }
+      checkOSVersion();
+    } else {
+      res.status(500).json({
+        error: 'No image name provided',
+      });
+    }
+  }
+
 }
 
 export default new MiscController();
